edbcdef6743a68e41ecfb5f376c07a4f30557197
Siddharth Kolte <siddharth.kolte@gmail.com>
2025-05-06 12:04:29 +0530

    Fixed the Model for Unwrap and some parts for smoothing. Added GLTF Materials, Added options to disable some effects to test for a low end PC.

--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
 import './style.css'
 import * as THREE from 'three';
-import { GLTFLoader, OrbitControls, RGBELoader, EffectComposer, OutputPass, SSRPass, SSAARenderPass, GTAOPass } from 'three/examples/jsm/Addons.js';
+import { GLTFLoader, OrbitControls, RGBELoader, EffectComposer, OutputPass, SSRPass, SSAARenderPass, GTAOPass, RenderPass } from 'three/examples/jsm/Addons.js';
 import { SetupPCSSShadow } from './core/pcssShadow.js';
+import { renderOptions } from './runtime/settings.js';
 
-SetupPCSSShadow();
+if (renderOptions.PCSSShadow) SetupPCSSShadow();
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
@@ -21,55 +22,65 @@ document.body.appendChild( renderer.domElement );
 
 const composer = new EffectComposer(renderer);
 
-const ssaaPass = new SSAARenderPass(scene, camera);
-ssaaPass.clearAlpha = true;
-composer.addPass( ssaaPass );
+if (renderOptions.SSAAPass) {
+  const ssaaPass = new SSAARenderPass(scene, camera);
+  ssaaPass.clearAlpha = true;
+  composer.addPass( ssaaPass );
+}
 
-const ssrPass = new SSRPass({renderer, scene, camera, width:size.width, height:size.height});
-ssrPass.beautyRenderTarget.samples = 8;
-ssrPass.maxDistance = 2.5;
-ssrPass.blur = true;
-ssrPass.fresnel = true;
-composer.addPass(ssrPass);
+if (renderOptions.SSRPass) {
+  const ssrPass = new SSRPass({renderer, scene, camera, width:size.width, height:size.height});
+  ssrPass.beautyRenderTarget.samples = 8;
+  ssrPass.maxDistance = 2.5;
+  ssrPass.blur = true;
+  ssrPass.fresnel = true;
+  composer.addPass(ssrPass);
+}
 
-const gtaoPass = new GTAOPass( scene, camera, size.width, size.height );
-gtaoPass.output = GTAOPass.OUTPUT.Default;
-const aoParameters = {
-  radius: 0.25,
-  distanceExponent: 1.,
-  thickness: 0.1,
-  scale: 2.,
-  samples: 16,
-  distanceFallOff: 1.,
-  screenSpaceRadius: false,
-};
-const pdParameters = {
-  lumaPhi: 10.,
-  depthPhi: 2.,
-  normalPhi: 3.,
-  radius: 0.,
-  radiusExponent: 1.,
-  rings: 2.,
-  samples: 16,
-};
+if (renderOptions.GTAOPass) {
+  if (!renderOptions.SSAAPass || !renderOptions.SSRPass) composer.addPass(new RenderPass(scene, camera));
 
-gtaoPass.updateGtaoMaterial(aoParameters);
-gtaoPass.updatePdMaterial( pdParameters );
-composer.addPass( gtaoPass );
+  const gtaoPass = new GTAOPass( scene, camera, size.width, size.height );
+  gtaoPass.output = GTAOPass.OUTPUT.Default;
+  const aoParameters = {
+    radius: 0.25,
+    distanceExponent: 1.,
+    thickness: 0.1,
+    scale: 2.,
+    samples: 16,
+    distanceFallOff: 1.,
+    screenSpaceRadius: false,
+  };
+  const pdParameters = {
+    lumaPhi: 10.,
+    depthPhi: 2.,
+    normalPhi: 3.,
+    radius: 0.,
+    radiusExponent: 1.,
+    rings: 2.,
+    samples: 16,
+  };
 
-composer.addPass(new OutputPass());
+  gtaoPass.updateGtaoMaterial(aoParameters);
+  gtaoPass.updatePdMaterial( pdParameters );
+  composer.addPass( gtaoPass );
+}
+
+if (renderOptions.SSAAPass || renderOptions.SSRPass || renderOptions.GTAOPass) composer.addPass(new OutputPass());
+else {
+  composer.addPass(new RenderPass(scene, camera));
+}
 
 const hdriLoader = new RGBELoader();
 hdriLoader.load("/textures/hdri/city.hdr", (texData) => {
   texData.mapping = THREE.EquirectangularReflectionMapping;
   scene.environment = texData;
-  scene.environmentIntensity = 1.0;
+  scene.environmentIntensity = 0.75;
 }, null, (error) => console.log(error));
 
 const loader = new GLTFLoader();
-loader.load("/pump/scene.gltf", (data) => {
-  data.scene.position.set(0,-10,-15);
-  data.scene.scale.set(0.1,0.1,0.1);
+loader.load("/pump/Pump_02.gltf", (data) => {
+  data.scene.position.set(1.5,0,0);
   data.scene.traverse((obj) => {
     if (obj instanceof THREE.Mesh) {
       obj.castShadow = true;
@@ -80,14 +91,14 @@ loader.load("/pump/scene.gltf", (data) => {
   scene.add(data.scene);
 }, null, (error) => console.log(error));
 
-const light = new THREE.DirectionalLight( 0xffffff, 1.318 );
-light.position.set( 0, 20, 0 ); //default; light shining from top
+const light = new THREE.DirectionalLight( 0xffffff, 1. );
+light.position.set( 0, 5, 0 ); //default; light shining from top
 light.castShadow = true; // default false
 scene.add( light );
 
-light.shadow.normalBias = 1;
+light.shadow.normalBias = 1.;
 
-const d = 30;
+const d = 10;
 
 light.shadow.camera.left = -d;
 light.shadow.camera.right = d;
@@ -103,8 +114,8 @@ light.shadow.bias = 0.001;
 light.shadow.blurSamples = 4;
 
 const orbitControls = new OrbitControls(camera, renderer.domElement);
-orbitControls.maxDistance = 30;
-orbitControls.minDistance = 20;
+orbitControls.maxDistance = 10;
+orbitControls.minDistance = 8;
 orbitControls.enablePan = false;
 
 camera.position.set(-30,15,30);
---
+++ src/runtime/settings.js
@@ -0,0 +1,8 @@
+import * as THREE from 'three'
+
+export const renderOptions = {
+    PCSSShadow: true,
+    SSAAPass: true,
+    SSRPass: true,
+    GTAOPass: true
+}
